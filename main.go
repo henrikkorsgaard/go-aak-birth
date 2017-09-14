795507e1c64e92f9e3449daa04d7eedd8dbb6019
Henrik Korsgaard <korsgaard@cs.au.dk>
2017-09-14 18:04:08 +0200

    adding the first elements

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("wut")
+}
+
+func getData(id string) {
+	//https://www.odaa.dk/api/3/action/datastore_search?resource_id=2f981f6a-33a9-4ce6-a977-d3a88b68d856&limit=443
+
+}
